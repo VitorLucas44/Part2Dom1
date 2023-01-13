f1ca7246f7615fee13fe0902c620f592497dedfd
VitorLucas44 <vitoriosolucas@hotmail.com>
2023-01-13 09:44:12 +0100

    exo1

---
+++ main.js
@@ -0,0 +1,5 @@
+document.getElementsByClassName("tartine")[0].textContent;
+
+document.getElementsById("toast")[0].textContent;
+
+document.getElementsByClassName("tartine")[document.getElementsByClassName("tartine").length - 1];
